30ac361d132a48576da0d49de8aeaab7fcf96192
kaunjovi <kaunjovi@gmail.com>
2017-03-12 20:22:29 +0530

    first failing test.

---
+++ src/test/java/coding/problems/vendingmachine/TestVendingMachine.java
@@ -0,0 +1,14 @@
+package coding.problems.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestVendingMachine {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
